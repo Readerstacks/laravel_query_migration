23eb22f9957033b52b9bfa176458c7da6f7a6cb4
aman <ajain@abnosoftwares.co.ke>
2022-02-03 18:53:37 +0530

    new way to add query

--- src/Commands/MigrateCommand.php
+++ src/Commands/MigrateCommand.php
@@ -6,7 +6,7 @@ use Readerstacks\QueryMigration\Models\QueryMigration;
 
 class MigrateCommand extends Command {
 
-    protected $signature = 'QueryMigrate {status}  {--uid= : Run specific id only}  {--f : forecfully run again a migration}';
+    protected $signature = 'QueryMigrate {status} {--q= : query to save}  {--run : Run the query too}  {--uid= : Run specific id only}  {--f : forecfully run again a migration}';
 
     protected $description = 'Migrate custom queries';
 
@@ -27,6 +27,30 @@ class MigrateCommand extends Command {
          
             $this->startOneMigration( $this->option('uid'),$this->option('f'));
             $this->info('Completed migrations');
+         }
+         else if(strtolower($status)=="add"  ){
+            $this->info('Starting adding new');
+         
+            $this->addNewQuery( $this->option('q'),$this->option('run'));
+             
+         } 
+         else if(strtolower($status)=="remove" &&  $this->option('uid')!=NULL){
+            $this->info('Start removing');
+         
+            $this->removeUid( $this->option('uid'));
+            $this->info('Completed');
+         }  
+         else if( ($status)=="removeAll" ){
+            $this->info('Start removing');
+         
+            $this->removeAll();
+            $this->info('Completed');
+         }  
+         else if(strtolower($status)=="list" ){
+            
+         
+            $this->listAll( $this->option('uid'));
+          
          }   
          else if(strtolower($status)=="pending"){
             $this->info('Pending migrations');
@@ -36,6 +60,89 @@ class MigrateCommand extends Command {
          else{
             $this->error('Command Not Found');
          } 
+    }
+
+    public function listAll(){
+
+        $queries    =  config("query_migration_config.queries");
+        
+        $data=[];
+        foreach($queries as $k=>$qr){
+            $data[]=['Query'=>$qr['query'], 'Time'=>$qr['time'],'UID'=>$qr['uid']];
+            
+        }
+        $this->table(
+            ['Query', 'Time','UID'],
+            $data
+             
+        );
+
+    }
+    
+    public function removeAll(){
+
+        $query_migration_config    =  config("query_migration_config");
+        $query_migration_config["queries"]=[];
+       
+        file_put_contents(base_path()."/config/query_migration_config.php","<?php return ".var_export($query_migration_config,true).";");
+        $this->line("\n Removed All  ");   
+
+    }
+    public function removeUid($uid){
+
+        $query_migration_config    =  config("query_migration_config");
+        $queries=$query_migration_config["queries"];
+
+        foreach($queries as $k=>$qr){
+        
+            if($qr["uid"]==$uid){
+                $this->line("\n Query : ".$qr["query"]. " - UID :".$qr["uid"]." Removed   ");   
+                unset($query_migration_config["queries"][$k]);
+               
+                break;
+            }
+        }
+        file_put_contents(base_path()."/config/query_migration_config.php","<?php return ".var_export($query_migration_config,true).";");
+
+    }
+
+    public function addNewQuery($query,$insert=false){
+        $query = $this->ask('Enter query');
+      
+        $queries    =  config("query_migration_config");
+        $qArr       =  ["query"=>"$query","name"=>"UNKNOWN","time"=>time(),"uid"=>uniqid()];
+
+        $queries["queries"][]  =  $qArr;
+       
+        if($insert){
+            try{
+                \DB::unprepared($query);
+                $QueryMigration=new QueryMigration;
+                $QueryMigration->uid=$qArr["uid"];
+                $QueryMigration->name=$qArr["name"];
+                $QueryMigration->save();
+            
+                file_put_contents(base_path()."/config/query_migration_config.php","<?php return ".var_export($queries,true).";");
+                $this->success("\n Success!! Query : ".$query. " - UID :".$qArr["uid"]."   ");   
+                
+            }
+            catch(\Exception $e){
+                echo $e->getMessage() ." $query" ;
+
+                $this->error("\n". $e->getMessage() ." $query" );   
+            }
+        }
+        else{
+            $QueryMigration=new QueryMigration;
+            $QueryMigration->uid=$qArr["uid"];
+            $QueryMigration->name=$qArr["name"];
+            $QueryMigration->save();
+        
+            file_put_contents(base_path()."/config/query_migration_config.php","<?php return ".var_export($queries,true).";");
+            $this->line("\n  Success!! Query : ".$query. " - UID :".$qArr["uid"]."   ");   
+        }
+
+        
     }
 
     public function pendingMigration(){
@@ -150,7 +257,7 @@ class MigrateCommand extends Command {
                         $QueryMigration->uid=$qr["uid"];
                         $QueryMigration->name=$qr["name"];
                         $QueryMigration->save();
-                        $this->line($qr['query']." DONE ");
+                        $this->line("\n\n Query : ".$qr['query']." - UID : ".$qr["uid"]."  DONE ");
                     }
                 }
                 
